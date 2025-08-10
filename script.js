4e7d17073855dbbf034810292d4f2732fcd7443d
sovesttrololo <101511231+sovesttrololo@users.noreply.github.com>
2025-08-10 23:14:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -845,7 +845,7 @@ document.querySelectorAll('input').forEach(input => {
         const valueToStore = input.value;
         const keyToStoe = input.key;
             localStorage.setItem(input.id, valueToStore);
-         if (keyToStoe.trim() == 'EUR' || keyToStoe.trim() == 'USD' || valueToStore.trim() == 'EUR' || valueToStore.trim() == 'USD' ) {
+         if (keyToStoe == 'EUR' || keyToStoe == 'USD' || valueToStore == 'EUR' || valueToStore == 'USD' ) {
              localStorage.removeItem(input.id);
          }
         }
